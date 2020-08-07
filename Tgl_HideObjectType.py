d83a0f45f197759ae96f7f4452f0e7a00c06ddb1
kazuyuki kamada <cveldorado@gmail.com>
2020-08-07 20:37:09 +0900

    ライトは選択の孤立で必ず含める

--- Tgl_HideObjectType.py
+++ Tgl_HideObjectType.py
@@ -1,47 +1,33 @@
 import bpy
 from bpy.props import BoolProperty
 
-def Tgl_HideObjTyp():
-    #表示させたくないものはシャープでコメントアウトしておく
-    area = bpy.context.space_data
-    #area.show_object_viewport_mesh = not area.show_object_viewport_mesh
-    area.show_object_viewport_curve = not area.show_object_viewport_curve
-    area.show_object_viewport_surf = not area.show_object_viewport_surf
-    area.show_object_viewport_meta = not area.show_object_viewport_meta
-    area.show_object_viewport_font = not area.show_object_viewport_font
-    #area.show_object_viewport_hair = not area.show_object_viewport_hair
-    area.show_object_viewport_pointcloud = not area.show_object_viewport_pointcloud
-    area.show_object_viewport_volume = not area.show_object_viewport_volume
-    #area.show_object_viewport_grease_pencil = not area.show_object_viewport_grease_pencil
-    area.show_object_viewport_armature = not area.show_object_viewport_armature
-    #area.show_object_viewport_lattice = not area.show_object_viewport_lattice
-    #area.show_object_viewport_empty = not area.show_object_viewport_empty
-    area.show_object_viewport_light = not area.show_object_viewport_light
-    area.show_object_viewport_light_probe = not area.show_object_viewport_light_probe
-    area.show_object_viewport_camera = not area.show_object_viewport_camera
-    area.show_object_viewport_speaker = not area.show_object_viewport_speaker
 
 
-#表示トグルするかはユーザー側でプリファレンス上で選択させておく？
-#一括TrueとFalseメニューも必要か？
-def Tgl_DisplayObjTyp():
-    area = bpy.context.space_data
-    area.show_object_viewport_mesh = True
-    area.show_object_viewport_curve = True
-    area.show_object_viewport_surf = True
-    area.show_object_viewport_meta = True
-    area.show_object_viewport_font = True
-    area.show_object_viewport_hair = True
-    area.show_object_viewport_pointcloud = True
-    area.show_object_viewport_volume = True
-    area.show_object_viewport_grease_pencil = True
-    area.show_object_viewport_armature = True
-    area.show_object_viewport_lattice = True
-    area.show_object_viewport_empty = True
-    area.show_object_viewport_light = True
-    area.show_object_viewport_light_probe = True
-    area.show_object_viewport_camera = True
-    area.show_object_viewport_speaker = True
+def Tgl_HideOverray():
+    ovl = bpy.context.space_data.overlay
+    ovl.show_extras = not ovl.show_extras
+    ovl.show_relationship_lines = not ovl.show_relationship_lines
+    ovl.show_outline_selected = not ovl.show_outline_selected
+    ovl.show_object_origins = not ovl.show_object_origins
+    ovl.show_motion_paths = not ovl.show_motion_paths
+    ovl.show_bones = not ovl.show_bones
+
+    ovl.show_cursor = not ovl.show_cursor
+    ovl.show_annotation = not ovl.show_annotation
+
+def All_HideOverray():
+    ovl = bpy.context.space_data.overlay
+    ovl.show_extras = False
+    ovl.show_relationship_lines = False
+    ovl.show_outline_selected = False
+    ovl.show_object_origins_all = False
+    ovl.show_object_origins = False
+    ovl.show_motion_paths = False
+    ovl.show_bones = False
+
+    ovl.show_cursor = False
+    ovl.show_annotation = False
+
 
               
 class Tgl_HideObjectType_OT_object(bpy.types.Operator):
@@ -50,10 +36,11 @@ class Tgl_HideObjectType_OT_object(bpy.types.Operator):
  
     #処理本体
     def execute(self, context,):
-        #メッシュが表示になってなかったら一旦全部表示させる。
-        area = bpy.context.space_data
-        if area.show_object_viewport_mesh == False:
-            Tgl_DisplayObjTyp()
+        #ボーンが表示なら一旦すべて
+        # ハイド
+        ovl = bpy.context.space_data.overlay
+        if ovl.show_bones == True:
+            All_HideOverray()
         else:
-            Tgl_HideObjTyp()
+            Tgl_HideOverray()
         return {'FINISHED'}
--- si_Isolate.py
+++ si_Isolate.py
@@ -1,20 +1,45 @@
 import bpy
 
-class si_isolate_OT_object(bpy.types.Operator): #ローカルビューを非ズームで実行する
+
+#ライトと選択中オブジェクトの合体。名前だけ抽出
+def dontmovecam():
+    oLit = bpy.data.lights
+    oObj = bpy.context.selected_objects
+    oCol = list()
+
+    for o in oLit:
+        oCol.append(o.name)
+    for o in oObj:
+        oCol.append(o.name)
+        
+
+    for o in oCol:
+        obj = bpy.context.scene.objects.get(o)
+        if obj: obj.select_set(True)
+        
+    #アクティブビューの取得
+    area = bpy.context.space_data
+    rg3d = area.region_3d
+    vDis = rg3d.view_distance
+    vMrx = rg3d.view_matrix
+
+    #ローカルビューに切り替え後、リージョンに距離とマトリックス入れ直す
+    bpy.ops.view3d.localview()
+    rg3d.view_distance = vDis
+    rg3d.view_matrix = vMrx
+    
+    bpy.ops.object.select_all(action='DESELECT')
+    for o in oObj:
+        obj = bpy.context.scene.objects.get(o.name)
+        if obj: obj.select_set(True)
+
+
+class si_isolate_OT_object(bpy.types.Operator):
     bl_idname = "view3d.si_isolate"
     bl_label = "Isolate Selected"
-
+    
     def execute(self, context):
-        area = bpy.context.space_data
-        rg3d = area.region_3d
-        vDis = rg3d.view_distance
-        vMrx = rg3d.view_matrix
 
-        #ローカルビューに切り替え後、リージョンに距離とマトリックス入れ直す
-        bpy.ops.view3d.localview()
-        rg3d.view_distance = vDis
-        rg3d.view_matrix = vMrx
+        dontmovecam()
 
-        #ローカルビューじゃなければNONEを返す。IF分岐で利用する
-        #lovw = bpy.context.space_data.local_view
         return {'FINISHED'}
