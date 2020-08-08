9804499d687c83b5a600552f9adfe0fad03002a2
kazuyuki kamada <cveldorado@gmail.com>
2020-08-08 22:06:29 +0900

    カーブやサーフェイスにも対応

--- si_MoveComponent.py
+++ si_MoveComponent.py
@@ -1,14 +1,12 @@
 import bpy
 
-D   ="DISABLE"
-E   ="ENABLE"
-T   ="TOGGLE"
 S1  ="builtin.select"
 #背面選択アドオン「Box Select X-Ray」必須。
 #https://blenderartists.org/t/box-select-x-ray/1212316
 #なければデフォルトの物を選択ってやりたいけど未対応
 S2  ="mesh_tool.select_box_xray"
 S3  ="builtin.select_box"
+S4  ="builtin.move"
 
 def set_active_tool(tool_name):
     for area in bpy.context.screen.areas:
@@ -17,16 +15,30 @@ def set_active_tool(tool_name):
             override["space_data"] = area.spaces[0]
             override["area"] = area
             bpy.ops.wm.tool_set_by_id(override, name=tool_name)
+
+
+
+#選択オブジェクトの属性やモードによって選択を切り替える
+def si_MoveComponent(a,b,c,d):
+    oSel = bpy.context.selected_objects
+    for o in oSel:
+        print(o.type)
+        if o.type == "MESH":
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.context.tool_settings.mesh_select_mode = (a,b,c)
+            set_active_tool(d)
             
-
-
-def si_MoveComponent(a,b,c,d):    
-    mode = bpy.context.active_object.mode
-    bpy.ops.object.mode_set(mode='EDIT')
-    set_active_tool(d)
-    bpy.ops.mesh.select_mode(use_extend=True ,type="VERT",action= a )
-    bpy.ops.mesh.select_mode(use_extend=True ,type="EDGE",action= b )
-    bpy.ops.mesh.select_mode(use_extend=True ,type="FACE",action= c )
+        elif o.type == "CURVE" or "SURFACE":
+            bpy.ops.object.mode_set(mode='EDIT')
+            set_active_tool(S4)
+        
+        #アーマチュアやカーブなど、オブジェクトモードなら編集に入るが
+        #POSEモードなら何もしない。
+        elif bpy.context.mode == "POSE":
+            print("None")
+        
+        else:
+            bpy.ops.object.mode_set(mode='EDIT')
 
 
 class si_MoveComponent_OT_object(bpy.types.Operator):
@@ -36,7 +48,7 @@ class si_MoveComponent_OT_object(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'} 
 
     def execute(self, context,):
-        si_MoveComponent(E,E,E,S1)
+        si_MoveComponent(True,True,True,S1)
         return {'FINISHED'}
 
 class si_MoveComponent1_OT_object(bpy.types.Operator):
@@ -46,7 +58,7 @@ class si_MoveComponent1_OT_object(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'} 
 
     def execute(self, context,):
-        si_MoveComponent(E,D,D,S2)
+        si_MoveComponent(True,False,False,S2)
         return {'FINISHED'}
     
 class si_MoveComponent2_OT_object(bpy.types.Operator):
@@ -56,7 +68,7 @@ class si_MoveComponent2_OT_object(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'} 
 
     def execute(self, context,):
-        si_MoveComponent(D,E,D,S2)
+        si_MoveComponent(False,True,False,S2)
         return {'FINISHED'}
 
 class si_MoveComponent3_OT_object(bpy.types.Operator):
@@ -66,7 +78,5 @@ class si_MoveComponent3_OT_object(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'} 
 
     def execute(self, context,):
-        si_MoveComponent(D,D,E,S2)
+        si_MoveComponent(False,False,True,S2)
         return {'FINISHED'}
-
-
