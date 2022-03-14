8d019f3a01125410e290146d557c321df3c48a78
kazuyuki kamada <cveldorado@gmail.com>
2022-03-15 01:45:25 +0900

    メッシュとカーブのみSubdivision適応

--- si_MoveComponent.py
+++ si_MoveComponent.py
@@ -18,13 +18,13 @@ def set_active_tool(tool_name):
 
 
 #選択オブジェクトの属性やモードによって選択を切り替える
-def si_MoveComponent(a,b,c,d):
+def si_MoveComponent(a,b,c,select_tool):
     oSel = bpy.context.selected_objects
     for o in oSel:
         if o.type == "MESH":
             bpy.ops.object.mode_set(mode='EDIT')
             bpy.context.tool_settings.mesh_select_mode = (a,b,c)
-            set_active_tool(d)
+            set_active_tool(select_tool)
         elif o.type == "CURVE":
             bpy.ops.object.mode_set(mode='EDIT')
             set_active_tool(S4)
@@ -45,7 +45,7 @@ class si_MoveComponent_OT_object(bpy.types.Operator):
     bl_idname = "object.si_movecomponent"
     bl_label = "Move Component Tool"
     bl_description = "Move Component Tool like Softimage"
-    bl_options = {'REGISTER', 'UNDO'} 
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context,):
         si_MoveComponent(True,True,True,S1)
@@ -55,17 +55,17 @@ class si_MoveComponent1_OT_object(bpy.types.Operator):
     bl_idname = "object.si_movecomponent1"
     bl_label = "Point Select Tool"
     bl_description = "Point Select Tool like Softimage"
-    bl_options = {'REGISTER', 'UNDO'} 
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context,):
         si_MoveComponent(True,False,False,S2)
         return {'FINISHED'}
-    
+
 class si_MoveComponent2_OT_object(bpy.types.Operator):
     bl_idname = "object.si_movecomponent2"
     bl_label = "Edge Select Tool"
     bl_description = "Edge Select Tool like Softimage"
-    bl_options = {'REGISTER', 'UNDO'} 
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context,):
         si_MoveComponent(False,True,False,S2)
@@ -75,7 +75,7 @@ class si_MoveComponent3_OT_object(bpy.types.Operator):
     bl_idname = "object.si_movecomponent3"
     bl_label = "Polygon Select Tool"
     bl_description = "Polygon Select Tool like Softimage"
-    bl_options = {'REGISTER', 'UNDO'} 
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context,):
         si_MoveComponent(False,False,True,S2)
